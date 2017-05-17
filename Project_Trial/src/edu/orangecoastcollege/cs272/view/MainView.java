7a770bfa4bb96fe8008f68555c3e5d8061d0fd32
bwegener <bwegener@O074125-14V.COAST.CCCD.ADX>
2017-05-16 20:32:07 -0700

    trial 1.1

--- Project_Trial/src/edu/orangecoastcollege/cs272/view/MainView.java
+++ Project_Trial/src/edu/orangecoastcollege/cs272/view/MainView.java
@@ -1,17 +1,40 @@
 package edu.orangecoastcollege.cs272.view;
 
+import java.io.File;
+import java.time.Duration;
+
+import com.sun.media.jfxmedia.MediaPlayer;
+
 import javafx.application.Application;
+import javafx.scene.media.Media;
 import javafx.stage.Stage;
+
+
 
 public class MainView extends Application
 {
+//    static final String COMBAT_FOREST_PATH = "mp3/MUSIC.mp3";
+//    static final String INTRO_FOREST_PATH = "mp3/MUSIC2.mp3"
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         ViewNavigator.setStage(primaryStage);
         ViewNavigator.loadScene("Character Creator", ViewNavigator.CHARACTER_CREATOR);
+
+        /*
+        Media song = new Media(new File(MainView.COMBAT_FOREST_PATH).toURI().toString());
+        MediaPlayer player = new MediaPlayer(song);
+        player.setVolume(50);
+        player.setAutoPlay(true);
+        player.setOnEndOfMedia(new Runnable)
+        {
+            public void run() {
+                player.seek(Duration.ZERO);
+            }
+        }
+        */
     }
-    
+
     public static void main(String[] args)
     {
         launch(args);
